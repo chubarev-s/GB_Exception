bde35c59f835c5bc590bb86682202ea9d51634f0
chubarev.s <chubarev_sergey@mail.ru>
2023-08-31 22:56:12 +0500

    HW2

--- HW2/Task1.java
+++ HW2/Task1.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Task1 {
 
-//    public static void main(String[] args) {
-//        while (true) {
-//            try {
-//                Scanner iScanner = new Scanner(System.in);
-//                System.out.printf("Введите число n: ");
-//                float n = iScanner.nextFloat();
-//                System.out.printf("Введённое число:  %s \n", n);
-//            } catch (Exception ex) {
-//                System.out.println("Некорректный ввод числа, повторите попытку");
-//            }
-//        }
-//    }
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                Scanner iScanner = new Scanner(System.in);
+                System.out.printf("Введите число n: ");
+                float n = iScanner.nextFloat();
+                System.out.printf("Введённое число:  %s \n", n);
+            } catch (Exception ex) {
+                System.out.println("Некорректный ввод числа, повторите попытку");
+            }
+        }
+    }
 }
